e0488df7a8c1db81397c613d576ab66a1e069e49
James Pearce <github@tripleodeon.com>
2024-11-17 22:51:15 -0500

    Constructor and persister

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,10 @@ import {
 } from 'tinybase/synchronizers/synchronizer-ws-server-durable-object';
 
 export class TinyBaseDurableObject extends WsServerDurableObject {
+  constructor(ctx: any, env: any) {
+    super(ctx, env);
+  }
+
   onPathId(pathId: Id, addedOrRemoved: IdAddedOrRemoved) {
     console.info((addedOrRemoved ? 'Added' : 'Removed') + ` path ${pathId}`);
   }
@@ -15,6 +19,16 @@ export class TinyBaseDurableObject extends WsServerDurableObject {
         ` client ${clientId} on path ${pathId}`,
     );
   }
+
+  // Uncomment this method to preserve data in the Durable Object between
+  // client sessions:
+
+  // createPersister() {
+  //   return createDurableObjectStoragePersister(
+  //     createMergeableStore(),
+  //     this.ctx.storage,
+  //   );
+  // }
 }
 
 export default {
