379958dd507dd737081b44fb7d901b5fb15aca82
James Pearce <github@tripleodeon.com>
2024-11-17 23:06:49 -0500

    Toggle storage

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,15 @@
-import {Id, IdAddedOrRemoved} from 'tinybase';
+import {createMergeableStore, Id, IdAddedOrRemoved} from 'tinybase';
+import {createDurableObjectStoragePersister} from 'tinybase/persisters/persister-durable-object-storage';
 import {
   getWsServerDurableObjectFetch,
   WsServerDurableObject,
 } from 'tinybase/synchronizers/synchronizer-ws-server-durable-object';
+
+// Whether to persist data in the Durable Object between client sessions.
+//
+// If false, the Durable Object only provides synchronization between clients
+// (which are assumed to persist their own data).
+const PERSIST_TO_DURABLE_OBJECT = false;
 
 export class TinyBaseDurableObject extends WsServerDurableObject {
   constructor(ctx: any, env: any) {
@@ -20,15 +27,14 @@ export class TinyBaseDurableObject extends WsServerDurableObject {
     );
   }
 
-  // Uncomment this method to preserve data in the Durable Object between
-  // client sessions:
-
-  // createPersister() {
-  //   return createDurableObjectStoragePersister(
-  //     createMergeableStore(),
-  //     this.ctx.storage,
-  //   );
-  // }
+  createPersister() {
+    if (PERSIST_TO_DURABLE_OBJECT) {
+      return createDurableObjectStoragePersister(
+        createMergeableStore(),
+        this.ctx.storage,
+      );
+    }
+  }
 }
 
 export default {
